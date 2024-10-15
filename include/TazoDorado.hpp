a594a90bb0611a6ffde4c2d3c7ea4f80dac31d93
ElArriaga17 <a22310240@live.ceti.mx>
2024-10-15 02:03:22 +0000

    clase1

---
+++ include/TazoDorado.hpp
@@ -0,0 +1,13 @@
+class TazoDorado
+{
+private:
+    bool flotando;
+public:
+    TazoDorado() {
+        this->flotando = false;
+    }
+    ~TazoDorado() {}
+    void flotar(){
+        this->flotando = true;
+    }
+};
